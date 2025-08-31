3a13642f3473d1c48a423c4a31b84be0f289383b
Pancake <pancakecontact12345@gmail.com>
2025-08-31 23:06:46 +0200

    ECS: macro overhaul, generational IDs, easier component addition

--- include/ecs.h
+++ include/ecs.h
@@ -1,34 +1,122 @@
+
 #ifndef ECS_H
 #define ECS_H
 
-#include "components/materialComponent.h"
-#include "ecscomponents.h"
-#include "ecscomponentmethods.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "components/transformcomponent.h"
+#include "components/materialComponent.h"
 #include "components/meshcomponent.h"
 
-DEFINE_VECTOR(Entity, EntityVector)
-DEFINE_COMPONENT_STORAGE(TransformComponent, TransformComponent)
-DEFINE_COMPONENT_STORAGE(MaterialComponent, MaterialComponent);
-DEFINE_COMPONENT_STORAGE(MeshComponent, MeshComponent)
+typedef uint32_t Entity;
+
+#define INDEX_BITS 24u
+#define INDEX_MASK ((1u << INDEX_BITS) - 1u)
+#define GENERATION_BITS (32u - INDEX_BITS)
+#define GEN_MASK ((1u << GENERATION_BITS) - 1u)
+
+#define MAKE_ENTITY(index, gen) (Entity)((((uint32_t)(gen) & GEN_MASK) << INDEX_BITS) | ((uint32_t)(index) & INDEX_MASK))
+#define ENTITY_INDEX(e) ((uint32_t)((e) & INDEX_MASK))
+#define ENTITY_GEN(e)   ((uint32_t)((e) >> INDEX_BITS))
+
+#define ECS_COMPONENTS \
+    X(TransformComponent) \
+    X(MaterialComponent)  \
+    X(MeshComponent)
+
+#define DEFINE_COMPONENT_STORAGE(Type)                               \
+    typedef struct {                                                 \
+        Type **items;                                                \
+        size_t capacity;                                             \
+    } Type##Storage;                                                  \
+                                                                      \
+    static inline Type##Storage* Type##Storage_create(void) {         \
+        Type##Storage* s = malloc(sizeof(*s));                       \
+        if (!s) return NULL;                                          \
+        s->items = NULL;                                              \
+        s->capacity = 0;                                              \
+        return s;                                                     \
+    }                                                                 \
+                                                                      \
+    static inline void Type##Storage_free(Type##Storage* s) {         \
+        if (!s) return;                                               \
+        for (size_t i = 0; i < s->capacity; ++i) {                    \
+            if (s->items[i]) free(s->items[i]);                       \
+        }                                                              \
+        free(s->items);                                               \
+        free(s);                                                      \
+    }                                                                 \
+                                                                      \
+    static inline int Type##Storage_ensure(Type##Storage* s, size_t idx) { \
+        if (idx < s->capacity) return 0;                              \
+        size_t newcap = s->capacity ? s->capacity * 2 : 16;           \
+        while (newcap <= idx) newcap *= 2;                            \
+        Type **tmp = realloc(s->items, newcap * sizeof(Type*));       \
+        if (!tmp) return -1;                                          \
+        for (size_t i = s->capacity; i < newcap; ++i) tmp[i] = NULL;  \
+        s->items = tmp;                                               \
+        s->capacity = newcap;                                         \
+        return 0;                                                     \
+    }                                                                 \
+                                                                      \
+    static inline int Type##Storage_add(Type##Storage* s, Entity e, Type* comp) { \
+        if (!s) return -1;                                            \
+        size_t idx = ENTITY_INDEX(e);                                 \
+        if (Type##Storage_ensure(s, idx) != 0) return -1;             \
+        if (s->items[idx]) free(s->items[idx]);                       \
+        s->items[idx] = comp;                                         \
+        return 0;                                                     \
+    }                                                                 \
+                                                                      \
+    static inline Type* Type##Storage_get(Type##Storage* s, Entity e) { \
+        if (!s) return NULL;                                          \
+        size_t idx = ENTITY_INDEX(e);                                 \
+        if (idx >= s->capacity) return NULL;                          \
+        return s->items[idx];                                         \
+    }                                                                 \
+                                                                      \
+    static inline void Type##Storage_remove(Type##Storage* s, Entity e) { \
+        if (!s) return;                                               \
+        size_t idx = ENTITY_INDEX(e);                                 \
+        if (idx >= s->capacity) return;                               \
+        if (s->items[idx]) { free(s->items[idx]); s->items[idx] = NULL; } \
+    }
+
+#define X(T) DEFINE_COMPONENT_STORAGE(T)
+ECS_COMPONENTS
+#undef X
 
 typedef struct ECS {
-    Entity nextEntity;
-    EntityVector* entities;
-
-    TransformComponentStorage* transforms;
-    MaterialComponentStorage* materials;
-    MeshComponentStorage* meshes;
+    uint32_t next_index;
+    uint32_t *free_indices;
+    size_t free_count;
+    size_t free_capacity;
+    uint32_t *generations;
+    size_t index_capacity;
+    Entity *entities;
+    size_t entity_count;
+    size_t entity_capacity;
+#define X(T) T##Storage* T##_storage;
+    ECS_COMPONENTS
+#undef X
 } ECS;
 
-DEFINE_ECS_COMPONENT_METHODS(TransformComponent, transforms);
-DEFINE_ECS_COMPONENT_METHODS(MaterialComponent, materials);
-DEFINE_ECS_COMPONENT_METHODS(MeshComponent, meshes);
+int EntityList_ensure_capacity(ECS* ecs, size_t idx);
 
-ECS* ECS_Create();
+#define X(T) \
+    int ECS_Add##T(ECS* ecs, Entity e, T* comp); \
+    T* ECS_Get##T(ECS* ecs, Entity e); \
+    void ECS_Remove##T(ECS* ecs, Entity e);
+ECS_COMPONENTS
+#undef X
+
+ECS* ECS_Create(void);
 void ECS_Destroy(ECS* ecs);
-
 Entity ECS_CreateEntity(ECS* ecs);
 void ECS_DestroyEntity(ECS* ecs, Entity e);
+int Entity_IsAlive(ECS* ecs, Entity e);
 
 #endif
+
--- src/ecs.c
+++ src/ecs.c
@@ -1,71 +1,113 @@
 #include "ecs.h"
-#include "components/materialComponent.h"
-#include <stdlib.h>
 
-ECS* ECS_Create() {
-    ECS* ecs = malloc(sizeof(ECS));
+int EntityList_ensure_capacity(ECS* ecs, size_t idx) {
+    if (idx < ecs->entity_capacity) return 0;
+    size_t nc = ecs->entity_capacity ? ecs->entity_capacity * 2 : 16;
+    while (nc <= idx) nc *= 2;
+    Entity *tmp = realloc(ecs->entities, nc * sizeof(Entity));
+    if (!tmp) return -1;
+    ecs->entities = tmp;
+    ecs->entity_capacity = nc;
+    return 0;
+}
+
+#define X(T) \
+    int ECS_Add##T(ECS* ecs, Entity e, T* comp) { return T##Storage_add(ecs->T##_storage, e, comp); } \
+    T* ECS_Get##T(ECS* ecs, Entity e) { return T##Storage_get(ecs->T##_storage, e); } \
+    void ECS_Remove##T(ECS* ecs, Entity e) { T##Storage_remove(ecs->T##_storage, e); }
+ECS_COMPONENTS
+#undef X
+
+static int ensure_index_capacity(ECS *ecs, uint32_t idx) {
+    if (idx < ecs->index_capacity) return 0;
+    size_t nc = ecs->index_capacity ? ecs->index_capacity * 2 : 16;
+    while (nc <= idx) nc *= 2;
+    uint32_t *g = realloc(ecs->generations, nc * sizeof(uint32_t));
+    if (!g) return -1;
+    for (size_t i = ecs->index_capacity; i < nc; ++i) g[i] = 0;
+    ecs->generations = g;
+    ecs->index_capacity = nc;
+    return 0;
+}
+
+static int push_free_index(ECS *ecs, uint32_t idx) {
+    if (ecs->free_count == ecs->free_capacity) {
+        size_t nc = ecs->free_capacity ? ecs->free_capacity * 2 : 16;
+        uint32_t *tmp = realloc(ecs->free_indices, nc * sizeof(uint32_t));
+        if (!tmp) return -1;
+        ecs->free_indices = tmp;
+        ecs->free_capacity = nc;
+    }
+    ecs->free_indices[ecs->free_count++] = idx;
+    return 0;
+}
+
+ECS* ECS_Create(void) {
+    ECS* ecs = malloc(sizeof(*ecs));
     if (!ecs) return NULL;
-
-    ecs->nextEntity = 0;
-    ecs->entities   = EntityVector_create();
-    ecs->transforms = TransformComponentStorage_create();
-    ecs->materials = MaterialComponentStorage_create();
-    ecs->meshes     = MeshComponentStorage_create();
+    ecs->next_index = 0;
+    ecs->free_indices = NULL;
+    ecs->free_count = 0;
+    ecs->free_capacity = 0;
+    ecs->generations = NULL;
+    ecs->index_capacity = 0;
+    ecs->entities = NULL;
+    ecs->entity_count = 0;
+    ecs->entity_capacity = 0;
+#define X(T) ecs->T##_storage = T##Storage_create();
+    ECS_COMPONENTS
+#undef X
     return ecs;
 }
 
 void ECS_Destroy(ECS* ecs) {
     if (!ecs) return;
-
-    TransformComponentStorage_free(ecs->transforms);
-    MeshComponentStorage_free(ecs->meshes);
-    EntityVector_free(ecs->entities);
+#define X(T) T##Storage_free(ecs->T##_storage);
+    ECS_COMPONENTS
+#undef X
+    free(ecs->free_indices);
+    free(ecs->generations);
+    free(ecs->entities);
     free(ecs);
 }
 
 Entity ECS_CreateEntity(ECS* ecs) {
-    Entity e = ecs->nextEntity++;
-    EntityVector_push(ecs->entities, e);
+    uint32_t idx;
+    if (ecs->free_count > 0) {
+        idx = ecs->free_indices[--ecs->free_count];
+    } else {
+        idx = ecs->next_index++;
+        if (ensure_index_capacity(ecs, idx) != 0) return UINT32_MAX;
+    }
+    uint32_t gen = ecs->generations[idx];
+    Entity e = MAKE_ENTITY(idx, gen);
+    if (EntityList_ensure_capacity(ecs, ecs->entity_count) == 0) {
+        ecs->entities[ecs->entity_count++] = e;
+    }
     return e;
 }
 
 void ECS_DestroyEntity(ECS* ecs, Entity e) {
-    TransformComponentStorage_remove(ecs->transforms, e);
-    MeshComponentStorage_remove(ecs->meshes, e);
-
-    for (size_t i = 0; i < ecs->entities->count; i++) {
-        if (ecs->entities->data[i] == e) {
-            ecs->entities->data[i] = ecs->entities->data[ecs->entities->count - 1];
-            ecs->entities->count--;
-            return;
+    uint32_t idx = ENTITY_INDEX(e);
+    if (idx >= ecs->index_capacity) return;
+    if (ecs->generations[idx] != ENTITY_GEN(e)) return;
+#define X(T) ECS_Remove##T(ecs, e);
+    ECS_COMPONENTS
+#undef X
+    ecs->generations[idx] = (ecs->generations[idx] + 1) & (uint32_t)GEN_MASK;
+    push_free_index(ecs, idx);
+    for (size_t i = 0; i < ecs->entity_count; ++i) {
+        if (ecs->entities[i] == e) {
+            ecs->entities[i] = ecs->entities[ecs->entity_count - 1];
+            ecs->entity_count--;
+            break;
         }
     }
 }
 
-void ECS_AddTransform(ECS* ecs, Entity e, TransformComponent* comp) {
-    TransformComponentStorage_add(ecs->transforms, e, comp);
-}
-
-TransformComponent* ECS_GetTransform(ECS* ecs, Entity e) {
-    TransformComponent** ptr = TransformComponentStorage_get(ecs->transforms, e);
-    return ptr ? *ptr : NULL;
-}
-
-void ECS_AddMaterial(ECS* ecs, Entity e, MaterialComponent* comp) {
-    MaterialComponentStorage_add(ecs->materials, e, comp);
-}
-
-MaterialComponent* ECS_GetMaterial(ECS* ecs, Entity e) {
-    MaterialComponent** ptr = MaterialComponentStorage_get(ecs->materials, e);
-    return ptr ? *ptr : NULL;
-}
-
-void ECS_AddMesh(ECS* ecs, Entity e, MeshComponent* comp) {
-    MeshComponentStorage_add(ecs->meshes, e, comp);
-}
-
-MeshComponent* ECS_GetMesh(ECS* ecs, Entity e) {
-    MeshComponent** ptr = MeshComponentStorage_get(ecs->meshes, e);
-    return ptr ? *ptr : NULL;
+int Entity_IsAlive(ECS* ecs, Entity e) {
+    uint32_t idx = ENTITY_INDEX(e);
+    if (idx >= ecs->index_capacity) return 0;
+    return ecs->generations[idx] == ENTITY_GEN(e);
 }
 
